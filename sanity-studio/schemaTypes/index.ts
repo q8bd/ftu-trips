d1107562ae759ef587c0e1802f14cdf25aed10c5
q8bd <alaa.k.bq@gmail.com>
2026-02-26 13:26:46 +0300

    Create index.ts

---
+++ sanity-studio/schemaTypes/index.ts
@@ -0,0 +1,13 @@
+import trip from './trip'
+import itineraryDay from './itineraryDay'
+import accommodation from './accommodation'
+import recommendation from './recommendation'
+import siteSettings from './siteSettings'
+
+export const schemaTypes = [
+  trip,
+  itineraryDay,
+  accommodation,
+  recommendation,
+  siteSettings,
+]
